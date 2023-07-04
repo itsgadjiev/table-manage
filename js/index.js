62e96f87964051d91a45d1108dff27d5840812ae
JekaH1 <ceyhun100203@gmail.com>
2023-07-04 15:55:10 +0400

    task done

--- js/index.js
+++ js/index.js
@@ -3,38 +3,147 @@ const surnameInput = document.querySelector('.surname-input');
 const salaryInput = document.querySelector('.salary-input');
 const addBtn = document.querySelector('.add-btn');
 const tableInner = document.querySelector('.table-inner');
+const nameFilterInput = document.querySelector('.name-filter');
+const surnameFilterInput = document.querySelector('.surname-filter');
+const salaryMinFilterInput = document.querySelector('.salary-min-filter');
+const salaryMaxFilterInput = document.querySelector('.salary-max-filter');
+const salaryFilters = document.querySelectorAll('.salary-filter');
+
+
 const datas = [];
 
 
 function addToTable() {
-
     addBtn.addEventListener("click", () => {
         const inputElement = {
             name: `${nameInput.value}`,
             surname: `${surnameInput.value}`,
             salary: `${salaryInput.value}`
 
-
         }
         datas.push(inputElement);
-        console.log(datas);
+
         nameInput.value = '';
         surnameInput.value = '';
         salaryInput.value = '';
         tableInner.innerHTML = '';
 
-        for (let i = 0; i < datas.length; i++) {
+        datas.forEach((element, idx) => {
             tableInner.innerHTML += `<tr>
-            <th scope="row">${i + 1} </th>
-            <td>${datas[i].name}</td>
-            <td>${datas[i].surname}</td>
-            <td>${datas[i].salary}</td>
-           
+            <th scope="row">${idx + 1} </th>
+            <td>${element.name}</td>
+            <td>${element.surname}</td>
+            <td>${element.salary}</td>
         </tr>`
+        });
 
-        }
 
+        nameFilterInput.addEventListener('change', () => {
+            const filterData = nameFilterInput.value.toLowerCase();
+            const filteredData = datas.filter(element => {
+                const name = element.name.toLowerCase();
+                return name.includes(filterData);
+            });
+
+            tableInner.innerHTML = '';
+
+            filteredData.forEach((element, idx) => {
+                tableInner.innerHTML += `<tr>
+                <th scope="row">${idx + 1}</th>
+                <td>${element.name}</td>
+                <td>${element.surname}</td>
+                <td>${element.salary}</td>
+              </tr>`;
+            });
+        });
+
+
+        surnameFilterInput.addEventListener('change', () => {
+            const filterData = surnameFilterInput.value.toLowerCase();
+            const filteredSurnameData = datas.filter(element => {
+                const surname = element.surname.toLowerCase();
+                return surname.includes(filterData);
+            })
+            tableInner.innerHTML = '';
+
+            filteredSurnameData.forEach((element, idx) => {
+                tableInner.innerHTML += `<tr>
+                <th scope="row">${idx + 1}</th>
+                <td>${element.name}</td>
+                <td>${element.surname}</td>
+                <td>${element.salary}</td>
+              </tr>`;
+            });
+        })
+
+        salaryFilters.forEach(element => {
+            element.addEventListener('change', () => {
+                if (salaryMinFilterInput.value && salaryMaxFilterInput.value) {
+                    const filteredSalaryData = datas.filter(element => {
+                        return Number(element.salary) >= Number(salaryMinFilterInput.value) && Number(element.salary) <= Number(salaryMaxFilterInput.value);
+                    })
+
+                    tableInner.innerHTML = '';
+
+                    filteredSalaryData.forEach((element, idx) => {
+                        tableInner.innerHTML += `<tr>
+                        <th scope="row">${idx + 1}</th>
+                        <td>${element.name}</td>
+                        <td>${element.surname}</td>
+                        <td>${element.salary}</td>
+                      </tr>`;
+                    });
+                }
+                else if (salaryMinFilterInput.value) {
+                    const filteredSalaryData = datas.filter(element => {
+                        return Number(element.salary) >= Number(salaryMinFilterInput.value);
+                    })
+
+                    tableInner.innerHTML = '';
+
+                    filteredSalaryData.forEach((element, idx) => {
+                        tableInner.innerHTML += `<tr>
+                        <th scope="row">${idx + 1}</th>
+                        <td>${element.name}</td>
+                        <td>${element.surname}</td>
+                        <td>${element.salary}</td>
+                      </tr>`;
+                    });
+                }
+                else if (salaryMaxFilterInput.value) {
+                    const filteredSalaryData = datas.filter(element => {
+                        return Number(element.salary) <= Number(salaryMaxFilterInput.value);
+                    })
+
+                    tableInner.innerHTML = '';
+
+                    filteredSalaryData.forEach((element, idx) => {
+                        tableInner.innerHTML += `<tr>
+                        <th scope="row">${idx + 1}</th>
+                        <td>${element.name}</td>
+                        <td>${element.surname}</td>
+                        <td>${element.salary}</td>
+                      </tr>`;
+                    });
+                }
+                else {
+                    tableInner.innerHTML = '';
+                    datas.forEach((element, idx) => {
+                        tableInner.innerHTML += `<tr>
+                        <th scope="row">${idx + 1} </th>
+                        <td>${element.name}</td>
+                        <td>${element.surname}</td>
+                        <td>${element.salary}</td>
+                    </tr>`
+                    });
+                }
+            })
+        })
     })
-
 }
+
+console.log(datas);
+
+
 addToTable();
+
